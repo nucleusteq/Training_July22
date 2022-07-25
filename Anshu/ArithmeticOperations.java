06461ecd5d60f38734656856f2b4fb0470450e5d
Anshu Yadav <anshu.yadav@nucleusteq.com>
2022-07-25 16:54:28 +0530

    adding java files

---
+++ Anshu/ArithmeticOperations.java
@@ -0,0 +1,23 @@
+
+public class ArithmeticOperations {
+
+	public static void main(String[] args) {
+		int a,b,sum,subtract,product,division;
+		a = 10;
+		b = 25;
+		
+		sum = a+b;
+		product = a*b;
+		subtract = a-b;
+		division = a/b;
+		
+		System.out.println("Addition is:" +sum);
+		System.out.println("Subtraction is:" +subtract);
+		System.out.println("Product is:" +product);
+		System.out.println("Division is:" +division);
+		
+		
+
+	}
+
+}
