16844b826709eac94b3e10af2afb404fadf48d35
Himanshu Dhoke <himanshu.dhoke@nucleusteq.com>
2022-07-25 17:04:19 +0530

    Java Files Added!

---
+++ HimanshuRaipur/ArithmaticOperations.java
@@ -0,0 +1,22 @@
+
+public class ArithmaticOperations {
+
+	public static void main(String[] args) {
+		int a,b,sum,subtract,product,division;
+		a = 10;
+		b = 5;
+		
+		sum = a+b;
+		subtract = a-b;
+		product = a*b;
+		division = a/b;
+		
+		System.out.println("Addition is:" +sum);
+		System.out.println("Subtraction is:" +subtract);
+		System.out.println("Product is:" +product);
+		System.out.println("Division is:" +division);
+		
+
+	}
+
+}
