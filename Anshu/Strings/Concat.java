3d506d9a822aeb2d4304f1bac105e1945f21958c
Anshu Yadav <anshu.yadav@nucleusteq.com>
2022-07-25 23:56:30 +0530

    add java files

---
+++ Anshu/Strings/Concat.java
@@ -0,0 +1,15 @@
+
+public class Concat
+{  
+    public static void main(String[] args) {      
+        String str1 = "Hello";  
+        String str2 = "Anshu";  
+        String str3 = "Yadav";  
+        // Concatenating one string   
+        String str4 = str1.concat(str2);          
+        System.out.println(str4);  
+        // Concatenating multiple strings  
+        String str5 = str1.concat(str2).concat(str3);  
+        System.out.println(str5);  
+    }  
+}  
---
+++ Anshu/Strings/LowerString.java
@@ -0,0 +1,9 @@
+
+public class LowerString {
+
+	 public static void main(String args[])
+	{  
+	String s1="ANSHU YADAV";  
+	String s1lower=s1.toLowerCase();  
+	System.out.println(s1lower);  
+	}}
