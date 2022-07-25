987799ef5bf585f9bb2a00a76ed44ffaa035aadf
sagar-NucleusTeq <sagar.kalthiya@nucleusteq.com>
2022-07-25 19:43:04 +0530

    basic of java add array code

--- sagar/Basic of Java/Array/Array_Reversecoping.java
+++ sagar/Basic of Java/Array/Array_Reversecoping.java
@@ -1,5 +1,23 @@
 package Array;
+import java.util.*;
 
 public class Array_Reversecoping {
+    public static void main(String[] args) {
     
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the length of Array:-");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("\nEnter the Value of Array:-");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+        int[] rev =new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            rev[i] =arr[arr.length-i-1];
+        }
+        for (int i = 0; i < rev.length; i++) {
+            System.out.print(rev[i]+" ");
+        }
+    }
 }
--- sagar/Basic of Java/Array/array_intsertion.java
+++ sagar/Basic of Java/Array/array_intsertion.java
@@ -3,6 +3,16 @@ import java.util.*;
 
 public class array_intsertion {
     public static void main(String args[]){
-        
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the length of Array:-");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("\nEnter the Value of Array:-");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }  
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+"\n");
+        }    
     }
 }
