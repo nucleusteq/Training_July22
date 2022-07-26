e9f873f9809bc98f28e10f19018263b1ca417ea5
aayushi-nucleusteq <109508552+aayushi-nucleusteq@users.noreply.github.com>
2022-07-26 14:00:40 +0530

    Delete Aayushi_java_basics directory

--- Aayushi_java_basics/String/StringLC.java
+++
@@ -1,8 +0,0 @@
-import java.lang.String;
-public class StringLC
-{ public static void main(String args[])
-{  
-String s1="AAYUSHI";  
-String s1lower=s1.toLowerCase();  
-System.out.println(s1lower);  
-}}  
