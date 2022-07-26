69cc793be0a76ac702513b38d2c3d836a1a94124
aayushi gopawar <aayushi.gopawar@nucleusteq.com>
2022-07-27 00:43:07 +0530

    day2

---
+++ Aayushi/Aayushi_Day2/AbstractClass.java
@@ -0,0 +1,58 @@
+package interface_prgrm;
+
+abstract class Shape{
+	
+	abstract double perimeter();
+	abstract double area();
+	
+}
+class Rectangle extends Shape{
+	int length = 10, breadth = 5;
+	
+	
+	double perimeter() {
+		int perimeter = 2*(length+breadth); 
+		return perimeter;
+	}
+	
+	
+	double area() {
+		double area = length*breadth;
+		return area;
+	}
+
+
+	public String toString() {
+		return "Rectangle [length=" + length + ", breadth=" + breadth + "]";
+	}
+	
+	
+}
+class Circle extends Shape{
+	int r = 6;
+	
+	
+	double perimeter() {
+		double perimeter = 2*3.14*r;
+		return perimeter;
+	}
+
+	
+	
+	double area() {
+		double area = 3.14*r*r;
+		return area;
+	}
+	
+	
+}
+public class AbstractClass {
+	public static void main(String[] args) {
+		Circle c = new Circle();
+		System.out.println(c.area());
+		
+		Rectangle rect = new Rectangle();
+		System.out.println(rect.perimeter());
+	}
+
+}
