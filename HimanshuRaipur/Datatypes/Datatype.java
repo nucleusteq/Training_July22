f325569fb04172e59cf773e8b840def0cff25e3a
Himanshu Dhoke <himanshu.dhoke@nucleusteq.com>
2022-07-26 17:33:35 +0530

    Datatype Programs added!

---
+++ HimanshuRaipur/Datatypes/Datatype.java
@@ -0,0 +1,35 @@
+public class Datatype{
+
+	public static void main(String[] args){
+
+		byte byteNum=214; //storing integer value in byte
+		short shortNum=251.75; //storing double value in short
+		int intNum='H'; //storing character value in integer
+		long longNum=100; //storing integer in long
+		
+				
+		float floatNum=76488; //storing int value in float
+		double doubleNum=floatNum;// storing float value in double 
+
+		char ch=82;//storing int value in char
+	
+		boolean bool=0;//storing int value in boolean
+
+		System.out.println("Byte Data Type :"+byteNum);
+		System.out.println("Short Data Type :"+shortNum);
+		System.out.println("Int Data Type :"+intNum);
+		System.out.println("Long Data Type :"+longNum);
+		System.out.println();
+
+		System.out.println("Float Data Type :"+floatNum);
+		System.out.println("Double Data Type :"+doubleNum);
+
+		System.out.println();
+
+		System.out.println("Char Data Type :"+ch);
+		System.out.println();
+		System.out.println("Boolean Data Type :"+bool);	
+		
+
+	}
+}
