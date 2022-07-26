8c03c9e22d8e424b8cb9b8249dcfbf123fbdd865
sagar-NucleusTeq <sagar.kalthiya@nucleusteq.com>
2022-07-26 21:50:25 +0530

    day 2 second commit

---
+++ sagar/Day2/Java_Tranning/src/Oops_Programs/Bike_static_Program.java
@@ -0,0 +1,13 @@
+package Oops_Programs;
+
+public class Bike_static_Program {
+	static class Hero{
+		void display_bike() {
+			System.out.println("inside display bike");
+		}
+	public static void main(String[] args) {
+		Hero SI=new Hero();
+		SI.display_bike();
+	}	
+}
+}
