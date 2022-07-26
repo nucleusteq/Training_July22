f8a34b4d58cb5274600c6f71a76d5f7333cbfa7d
Prakriti Gupta <prakriti.gupta@nucleusteq.com>
2022-07-26 20:06:58 +0530

    Exercise 2 update

---
+++ Exercise2/Bike.java
@@ -0,0 +1,24 @@
+package student;
+
+public class Bike {
+	class Hero{
+		public void display() {
+			
+		}
+		
+	}
+	class Honda{
+		
+		public void show() {
+			
+		}
+	}
+	public static void main(String args[]) {
+		Bike o = new Bike();
+		Bike.Hero i = o.new Hero();
+		Bike.Honda j = o.new Honda();
+		
+	}
+	
+
+}
