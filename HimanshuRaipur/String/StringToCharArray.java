4c56708be74660b614b9c15eee749599efd326e9
Himanshu Dhoke <himanshu.dhoke@nucleusteq.com>
2022-07-26 16:26:29 +0530

    String Programs added!

---
+++ HimanshuRaipur/String/StringToCharArray.java
@@ -0,0 +1,11 @@
+public class StringToCharArray{
+
+	public static void main(String[] args){
+		String str="Welcome to NucleusTeq!";
+		char ch[]=str.toCharArray();
+		for(int i=0;i<ch.length;i++){
+			System.out.println(ch[i]);
+		}
+
+	}
+}
---
+++ HimanshuRaipur/String/StringToLowerCase.java
@@ -0,0 +1,7 @@
+public class StringToLowerCase{
+
+	public static void main(String[] args){
+		String str="Welcome To NucleusTeq";
+		System.out.println(str.toLowerCase());//This will print string in lower case.
+	}
+}
