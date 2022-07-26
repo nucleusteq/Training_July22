66f71fa0a293a3e136a3e4571400d475deec6d75
aayushi gopawar <aayushi.gopawar@nucleusteq.com>
2022-07-26 23:47:47 +0530

    day2

---
+++ Aayushi/Aayushi_Day2/nestedClass.java
@@ -0,0 +1,27 @@
+package interface_prgrm;
+
+class Bike{
+	class Hero{
+		
+		public void Display(){
+			System.out.println("class Hero");
+		}
+	}
+	class Honda{
+		public void Show(){
+			System.out.println("class Honda");
+		}
+	}
+}
+public class nestedClass {
+
+	public static void main(String[] args) {
+		
+		Bike.Hero hero = new Bike().new Hero();
+		hero.Display();
+		
+		Bike.Honda honda = new Bike().new Honda();
+		honda.Show();
+	}
+
+}
