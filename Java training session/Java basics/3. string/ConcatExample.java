f0efabe840f7a2bb7b884df35ad5f2f22431021a
himanshu learning <himanshu.chaurasiya@nucleusteq.com>
2022-07-26 09:12:26 +0530

    java basics

---
+++ Java training session/Java basics/3. string/ConcatExample.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+
+public class ConcatExample
+{  
+    public static void main(String[] args) {      
+        String str1 = "Hello";  
+        String str2 = "wolrd";  
+        String str3 = "brother";  
+        // Concatenating one string   
+        String str4 = str1.concat(str2);          
+        System.out.println(str4);  
+        // Concatenating multiple strings  
+        String str5 = str1.concat(str2).concat(str3);  
+        System.out.println(str5);  
+    }  
+}  
