584e6462063c6ce5641f0af55e40515cbd922fb2
Rashi Jain <rashi.jain@nucleusteq.com>
2022-07-27 20:48:23 +0530

    Error&Exception

---
+++ rashi/Day3ExceptionAnnotation/EmployeeField.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package annotations;
+
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+
+@Documented
+@Retention(CLASS)
+/**
+ * @author Rashi Jain
+ *
+ */
+public @interface EmployeeField {
+
+}
