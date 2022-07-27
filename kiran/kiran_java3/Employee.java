47eba9afd684f94a0790f05f8579aef4f34eccf4
Kiran <ndsai.kiran.nucleusteq@com>
2022-07-28 00:01:41 +0530

    half done

---
+++ kiran/kiran_java3/Employee.java
@@ -0,0 +1,21 @@
+import java.lang.module.ModuleDescriptor.Modifier;
+import java.lang.reflect.Field;
+
+public class Employee {
+public static void main(String args[]) {
+	EmployeeDetails emp1 = new EmployeeDetails();
+	emp1.setName("kiran"); 
+	emp1.setEmId(3);
+	emp1.setDepartment("Technical");
+	emp1.setEmail("employee3@gmail.com");
+	emp1.getEmpId(); 
+	Field[] fields;
+//	for (Field field : fields) {
+//        System.out.printf("%s %s %s%n",
+//            
+//            field.getName().getSimpleName(),
+//            field.getName()
+//        );
+//	 System.out.println("field name:"+name);
+}
+}
