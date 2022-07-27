5209b27acac2ce9a35f3532756f96bda46f66ef3
Anshu Yadav <anshu.yadav@nucleusteq.com>
2022-07-27 16:51:07 +0530

    completed

---
+++ Anshu/Anshu_java2/Class/StudentTest.java
@@ -0,0 +1,33 @@
+
+
+import java.util.Scanner;
+public class StudentTest{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		Students students[]=new Students[5];
+		for(int i=0;i<5;i++){
+			System.out.println("Enter Roll Number");
+			int rollNumber=sc.nextInt();
+			sc.nextLine();
+			System.out.println("Enter Name");
+			String name=sc.nextLine();
+			System.out.println("Enter Course");
+			String course=sc.nextLine();
+			System.out.println("Enter Marks1");
+			int marks1=sc.nextInt();
+			System.out.println("Enter Marks2");
+			int marks2=sc.nextInt();
+			System.out.println("Enter Marks3");
+			int marks3=sc.nextInt();
+			students[i]= new Students(rollNumber,name,course,marks1,marks2,marks3);
+			char grade=students[i].calculateGrade(marks1,marks2,marks3);
+			students[i].setGrade(grade);
+			System.out.println("Student Added");
+		}
+		System.out.println("Students Details");
+		for(Students student:students){
+			System.out.println(student);
+		}
+		
+	}
+}
