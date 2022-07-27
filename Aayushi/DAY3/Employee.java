c4d978f51af8d4ccabe2ec2562c7968e2712d6a1
aayushi gopawar <aayushi.gopawar@nucleusteq.com>
2022-07-27 18:43:45 +0530

    half way through

---
+++ Aayushi/DAY3/Employee.java
@@ -0,0 +1,18 @@
+
+public class Employee {
+
+
+	public static void main(String[] args){
+		EmployeeDetails ed= new EmployeeDetails();
+		ed.setName("aayushi");
+		ed.setEmId(6);
+		ed.setDepartment("t");
+		ed.setEmail("aayushi.com");
+		
+		for(Fields:fields , )
+		
+	//	System.out.println(ed.name);
+		//System.out.println(ed.department);
+	//	System.out.println(ed.empId);
+		
+	}}
