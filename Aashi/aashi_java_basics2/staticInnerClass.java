bd81f8b3c9797589b998172d84b3c110e9036abd
Aashi <aashi.agrawal@nucleusteq.com>
2022-07-27 16:06:19 +0530

    learning java in day2

---
+++ Aashi/aashi_java_basics2/staticInnerClass.java
@@ -0,0 +1,13 @@
+class Bikes{
+	static class Hero{
+		void show() {
+			System.out.println("inner class hero ");
+		}
+	}
+}
+public class staticInnerClass {
+	public static void main(String[] args) {
+		Bikes.Hero b = new Bikes.Hero();
+		b.show();
+		}
+}
