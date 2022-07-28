21fc372cfc2d218a85fc8f2550056be23ebf5b0b
Anshu Yadav <anshu.yadav@nucleusteq.com>
2022-07-28 15:37:21 +0530

    task completed

--- Anshu/JavaBasics -Day3/Employee.java
+++ Anshu/JavaBasics -Day3/Employee.java
@@ -1,20 +1,32 @@
 import java.lang.module.ModuleDescriptor.Modifier;
 import java.lang.reflect.Field;
-
 public class Employee {
-public static void main(String args[]) {
-	Employee ed = new Employee();
-	ed.setName("Anshu");
-	ed.setEmId(3);
-	ed.setDepartment("Technical");
-	ed.setEmail("employee3@gmail.com");
-	Field[] fields;
-//	for (Field field : fields) {
-//        System.out.printf("%s %s %s%n",
-//            
-//            field.getName().getSimpleName(),
-//            field.getName()
-//        );
-//	 System.out.println("feild name:"+name);
-}
-}
+	public static void main(String[] args) throws Exception {
+	
+		System.out.println();
+		
+		EmployeeDetails ed= new EmployeeDetails();
+		ed.setName("Anshu");
+		ed.setEmId(3);
+		ed.setDepartment("tech");
+		ed.setEmail("Anshu.com");
+		
+		for( Field field: ed.getClass().getDeclaredFields())
+		{
+			EmployeeField employeefield  = field.getAnnotation(EmployeeField.class);
+		System.out.println("field name" + "\t" + employeefield.name() );
+		
+		
+		field.setAccessible(true);
+		Object value = field.get(ed);
+		System.out.println("field value"+ "\t" + value);
+		
+		
+	        System.out.println("field type: " +"\t" + employeefield.type()  );
+	        System.out.println("field isPrimarykey: " +"\t" + employeefield.isPrimaryKey()  );
+	       
+	       }
+		System.out.println();
+		
+	}
+	}
--- Anshu/JavaBasics -Day3/EmployeeDetails.java
+++ Anshu/JavaBasics -Day3/EmployeeDetails.java
@@ -1,54 +1,59 @@
 import java.io.*;
 import java.util.Scanner;
-import java.lang.annotation.Retention;
+import java.lang.annotation.*;
 import java.lang.annotation.RetentionPolicy;
-
+import java.lang.annotation.Target;
+/**
+ *
+ * @author Anshu
+ *@version 1.0
+ *
+ */
+	@Target(ElementType.FIELD)
 	@Retention (RetentionPolicy.RUNTIME)
-	@interface EmployeeField 
+	
+	@interface EmployeeField
 	{
 	    String name();
-	    String type();
-	    String isPrimaryKey();
+	    Class<?> type();
+	    boolean isPrimaryKey() default false;
 	}
-	package Employee;
-
-	public class Employee {
-
+	class EmployeeDetails {
+	    //Creating properties of Employee class
+	@EmployeeField (name = "empId" , type=int.class ,isPrimaryKey=true)
+		int empId;
+	
+	@EmployeeField (name = "name", type=String.class)
+	String name;
+	@EmployeeField (name = "department", type=String.class)
+	String department;
+	@EmployeeField (name = "emailAddress", type=String.class)
+	String emailAddress;
+	 
+	//Getter and setters for getting and setting properties
+	public int getEmpId() {
+	    return empId;
 	}
-	class EmployeeDetails {  
-	    //Creating properties of Employee class  
-	int emp_id;  
-	String name, address, department, email;  
-	  
-	//Getter and setters for getting and setting properties  
-	public int getEmp_id() {  
-	    return emp_id;  
-	}  
-	public void setEmp_id(int emp_id) {  
-	    this.emp_id = emp_id;  
-	}  
-	public String getName() {  
-	    return name;  
-	}  
-	public void setName(String name) {  
-	    this.name = name;  
-	}  
-	public String getAddress() {  
-	    return address;  
-	}  
-	public void setAddress(String address) {  
-	    this.address = address;  
-	}  
-	public String getDepartment() {  
-	    return department;  
-	}  
-	public void setDepartment(String department) {  
-	    this.department = department;  
-	}  
-	public String getEmail() {  
-	    return email;  
-	}  
-	public void setEmail(String email) {  
-	    this.email = email;  
-	}  
+	public void setEmId(int emp_id) {
+	    this.empId = empId;
 	}
+	public String getName() {
+	    return name;
+	}
+	public void setName(String name) {
+	    this.name = name;
+	}
+	public String getDepartment(int a,int b) {
+	    return department;
+	}
+	public void setDepartment(String department) {
+	    this.department = department;
+	}
+	public String getEmail() {
+	    return emailAddress;
+	}
+	public void setEmail(String email) {
+	    this.emailAddress = emailAddress;
+	}
+	}
+	
