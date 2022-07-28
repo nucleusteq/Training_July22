f0c62931ffc1f959b32ce0c7f09efb99d3140990
himanshu learning <himanshu.chaurasiya@nucleusteq.com>
2022-07-28 11:27:02 +0530

    Employee completed

--- JavaTrainingSession/JavaException/EmployeeCode.java
+++ JavaTrainingSession/JavaException/EmployeeCode.java
@@ -1,32 +1,62 @@
 package JavaTrainingSession.JavaException;
 
-//@Retention(RetentionPolicy.EmployeeDetails)
-//@Target(ElementType.Field)
-//
-//
-//public @interface EmployeeField {
-//	String name default " ";
-//	String type default " ";
-//	Boolean isPrimaryKey default " ";
-//}
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.reflect.Field;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@interface EmployeeField{
+	String name();
+	Class< ?> type();
+	boolean isPrimaryKey();
+	}
 
 
-class EmployeeDetails
-{
-	String name;
-	int id;
-	String department;
-	String email;
+public class EmployeeCode {
+	public static void main(String[] args) throws Exception {
+		EmployeeDetails emp = new EmployeeDetails("himanshu",12,"MCA","hey@gmail.com");
+		
+		for(Field field:emp.getClass().getDeclaredFields()) {
+			EmployeeField efield=field.getAnnotation(EmployeeField.class);
+			System.out.println("field name "+ efield.name());	
+			 field.setAccessible(true);
+			Object value=field.get(emp);
+			 System.out.println("field value "+value );
+			 System.out.println("field type "+ efield.type());
+			 System.out.println("field isPrimaryKey "+ efield.isPrimaryKey());
+			 System.out.println("*****************");		
+		}
+	}
+}
+
+class EmployeeDetails{	
+	@EmployeeField(name="employee_name",type=String.class, isPrimaryKey=false)
+	private String employee_name;
+	
+	@EmployeeField(name="id",type=String.class, isPrimaryKey= true)
+	private int id;
+	
+	@EmployeeField(name="department",type=String.class, isPrimaryKey=false)
+	private String department;
+	
+	@EmployeeField(name="email",type=String.class, isPrimaryKey=false)
+	private String email;
+
+	
 	public EmployeeDetails(String name, int id,String department , String email)
 	{
-		this.name = name;
+		this.employee_name = name;
 		this.id = id;
 		this.department = department;
 		this.email = email;
 	}
+	
 	String getName()
 	{
-		return name;
+		return employee_name;
 	}
 	int getId()
 	{
@@ -42,11 +72,3 @@ class EmployeeDetails
 	}
 }
 
-
-public class EmployeeCode {
-	public static void main(String[] args) {
-		EmployeeDetails emp = new EmployeeDetails("hari",1,"mca","hari@gmail.com");
-		
-	}
-
-}
