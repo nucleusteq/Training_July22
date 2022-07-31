3780f225beeb6fae872462720efc784626842638
Himanshu Chaurasiya <109339866+himanshu-nucleus@users.noreply.github.com>
2022-07-31 11:20:39 +0530

    Revert "Himanshu chaurasiya"

--- JavaTrainingSession/JavaBasics/Introduction/JavaProg.java
+++
@@ -1,9 +0,0 @@
-package JavaTrainingSession.JavaBasics.Introduction;
-import java.lang.*;
-
-@SuppressWarnings("unused")
-public class JavaProg{
-	public static void main(String[] args){
-		System.out.println("Hello World");
-	}
-}
--- JavaTrainingSession/JavaBasics/String/StringLower.java
+++
@@ -1,8 +0,0 @@
-package JavaTrainingSession.JavaBasics.String;
-public class StringLower
-{ public static void main(String args[])
-{  
-String s1="GOINGLOWER";  
-String s1lower=s1.toLowerCase();  
-System.out.println(s1lower);  
-}}  
