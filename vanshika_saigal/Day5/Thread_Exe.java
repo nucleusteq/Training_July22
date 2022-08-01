92eb60cd3ad6eefe356fabb74d0004d1f46393e8
Vanshika Saigal <vanshika.saigal@nucleusteq.com>
2022-08-01 17:02:23 +0530

    ATM completed

---
+++ vanshika_saigal/Day5/Thread_Exe.java
@@ -0,0 +1,30 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Thread_Exe extends Thread implements Runnable {
+
+private Person person;
+
+public Thread_Exe(Person p) {
+    this.person = p;
+}
+public static void main(String[] args) {
+
+	Thread_Exe ts1 = new Thread_Exe(new Person("Aayushi"));
+    ts1.start();
+    Thread_Exe ts2 = new Thread_Exe(new Person("Reena"));
+    ts2.start();
+}
+@Override
+public void run() {
+    for (int i = 0; i < 3; i++) {
+        
+            ATM acc = ATM.getAccount(person);
+            acc.withdraw(100);           
+            if (acc.getBal() < 0) {
+                System.out.println("No amount in account");
+            }
+            acc.deposit(200);    
+    }
+    System.out.println("Final balance is Rs." + ATM.getBal());
+}}
