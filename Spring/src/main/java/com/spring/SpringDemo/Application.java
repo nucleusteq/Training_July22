be3a3d8c18a79ccaab025a159cd6db8e1c15d446
Himanshu Chaurasiya <himanshu@nucleusteq.com>
2022-08-03 20:32:06 +0530

    creating separate folder for spring

--- Spring/src/main/java/com/spring/SpringDemo/Application.java
+++ Spring/src/main/java/com/spring/SpringDemo/Application.java
@@ -1,9 +1,10 @@
 package com.spring.SpringDemo;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 		public static void main(String[] args) {
-			ClassPathXmlApplicationContext tempObj = new 
+			ClassPathXmlApplicationContext tempObj  = new 
 					ClassPathXmlApplicationContext("bean.xml");
 		      Student sch = tempObj.getBean("student",Student.class);
 		      System.out.println(sch);
