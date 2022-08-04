575a3635caf7d6d9c3aefa567807a1401116883a
Vanshika Saigal <vanshika.saigal@nucleusteq.com>
2022-08-04 23:56:57 +0530

    Spring Configuration

---
+++ vanshika_saigal/SpringExample/src/main/java/com/nt/training/mvc/SpringConfig.java
@@ -0,0 +1,33 @@
+package com.nt.training.mvc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+
+@EnableWebMvc
+@ComponentScan(basePackages = "com.nt.training.*")
+public class SpringConfig  extends WebMvcConfigurerAdapter{
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver internalResourceViewResolver=new InternalResourceViewResolver();
+		internalResourceViewResolver.setViewClass(JstlView.class);
+		internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+		internalResourceViewResolver.setSuffix(".jsp");
+		return internalResourceViewResolver;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		// TODO Auto-generated method stub
+		configurer.enable();
+	}
+	
+}
---
+++ vanshika_saigal/SpringExample/src/main/java/com/nt/training/mvc/WebServletConfiguration.java
@@ -0,0 +1,25 @@
+package com.nt.training.mvc;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebServletConfiguration implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext annotationConfigApplicationContext=new AnnotationConfigWebApplicationContext();
+		annotationConfigApplicationContext.register(SpringConfig.class);
+		annotationConfigApplicationContext.setServletContext(servletContext);
+		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(annotationConfigApplicationContext));
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+		
+	}
+
+}
