556d88f83304d96ce6b66619e967daad3e5a7b7a
sagar-NucleusTeq <sagar.kalthiya@nucleusteq.com>
2022-08-04 11:05:28 +0530

    All done

--- SagarKalthiya/ForME/spring/src/main/java/com/spring/firsst/lifecylce/LifeCons.java
+++ SagarKalthiya/ForME/spring/src/main/java/com/spring/firsst/lifecylce/LifeCons.java
@@ -1,6 +1,16 @@
 package com.spring.firsst.lifecylce;
 
-public class LifeCons {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class LifeCons implements BeanNameAware,BeanFactoryAware,ApplicationContextAware,InitializingBean, DisposableBean, BeanPostProcessor  {
 	   private String message;
 
 	   public void setMessage(String message){
@@ -15,4 +25,34 @@ public class LifeCons {
 	   public void destroy() {
 	      System.out.println("Bean will destroy now.");
 	   }
+	public void setBeanName(String name) {
+		// TODO Auto-generated method stub
+		System.out.println("SetBeanName is called :- "+name);
+		
+	}
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		// TODO Auto-generated method stub
+		System.out.println("SetBeanFactory is called :- " + beanFactory);
+		
+	}
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		// TODO Auto-generated method stub
+		System.out.println("setApplicationContext is Called :- " + applicationContext);
+		
+	}
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("After Property Set is called");
+		
+	}
+	
+	public Object postProcessBeforeInitialization(Object bean, String beanName) {
+		System.out.println("Post Process Before Initialization is called : Bean Name " +bean+ beanName );
+		return bean;
+	}
+	
+	public Object postProcessAfterInitialization(Object bean, String beanName){
+		System.out.println("Post Process After Initialization is called : Bean Name " +bean+ beanName);
+		return bean;
+	}
 	}
