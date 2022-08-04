ffaae69d3d9ed3bc08d3f4d8d568bc0495e40474
Shreya Punjabi <shreya.punjabi@nucleusteq.com>
2022-08-04 23:41:09 +0530

    SpringMvc

---
+++ shreya/SpringMvcExamples/src/main/java/com/nt/training/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.nt.training;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
+public class WelcomeController {
+	@RequestMapping(path= "/" , method=RequestMethod.GET)
+	public String getWelcome(ModelMap model){
+		model.addAttribute("message","Spring Mvn NT training");
+		return "welcome";
+		
+	}
+}
