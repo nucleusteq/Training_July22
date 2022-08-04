fbe2a4cc7883f379e6f19682160585a01328e63c
sandeepnucleus <sandeep.jain@nucleusteq.com>
2022-08-04 17:14:52 +0530

    Added spring mvc example

---
+++ Sandeep/SpringMvcExample/src/main/java/com/nt/training/HelloController.java
@@ -0,0 +1,25 @@
+package com.nt.training;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/" , method = RequestMethod.GET)
+	public String printWelcome(ModelMap modelMap) {
+		modelMap.addAttribute("message", "Spring MVC");
+		return "hello";
+	}
+	
+	
+	@RequestMapping(value="/processMessage" ,  method=RequestMethod.GET)
+	public String processIncomingMessage(@RequestParam("message") String message, ModelMap model) {
+		System.out.println("process message");
+		model.addAttribute("message", message);
+		return "message";
+	}
+}
---
+++ Sandeep/SpringMvcExample/src/main/webapp/WEB-INF/views/jsp/message.jsp
@@ -0,0 +1,3 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<div>Message is received ${message}</div>
