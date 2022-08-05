145556020827d97e04020c7c8ce46c11f44dae3f
Parth Waghela <Parth.waghela@nucleusteq.com>
2022-08-05 06:59:11 +0530

    Spring MVC Example

---
+++ ParthWaghela/Java_Tranning/Java_basics/SpringAnnotations/Application.java
@@ -0,0 +1,13 @@
+package com.mt.training.SpringAnnotations;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+public class Application {
+	public static void main(String[]args) {
+   	
+	AnnotationConfigApplicationContext	applicationcontext= new AnnotationConfigApplicationContext(SpringConfig.class);
+	
+	School s= applicationcontext.getBean(School.class);
+	System.out.println(s);
+	applicationcontext.close();	
+	}	
+}
--- ParthWaghela/Java_Tranning/Java_basics/SpringCore/java/com/mt/training/LifeCycle/LifeCycleBean.java
+++ ParthWaghela/Java_Tranning/Java_basics/SpringCore/java/com/mt/training/LifeCycle/LifeCycleBean.java
@@ -1,6 +1,16 @@
 package com.mt.training.LifeCycle;
 
-public class LifeCycleBean {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class LifeCycleBean implements BeanNameAware,BeanFactoryAware,ApplicationContextAware,InitializingBean, DisposableBean, BeanPostProcessor {
 	private String name;
 
 	public LifeCycleBean() {
@@ -25,4 +35,26 @@ public class LifeCycleBean {
 	public void destroy() {
 		System.out.println("Bean is destroyed now.");
 	}
+
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("afterProperties is getting called.");
+		
+	}
+
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		// TODO Auto-generated method stub
+		System.out.println("setApplicationContext is getting called");
+	}
+
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		// TODO Auto-generated method stub
+		System.out.println("setBeanFactory is getting called");
+	}
+
+	public void setBeanName(String name) {
+		// TODO Auto-generated method stub
+		System.out.println("setBeanName is getting called");
+	}
+	
 }
