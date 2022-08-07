0500f15c550d792a8f3b6f43940e5b4f1f20c7a3
Reena Anthony <reena.anthony@nucleusteq.com>
2022-08-07 22:55:09 +0530

    teacher commit

---
+++ reena_anthony_05_08/Teacher/src/main/java/com/nt/training/SpringBootApplication/controller/TeacherController.java
@@ -0,0 +1,50 @@
+package com.nt.training.SpringBootApplication.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nt.training.SpringBootApplication.domain.Teacher;
+import com.nt.training.SpringBootApplication.service.TeacherService;
+
+@RestController
+public class TeacherController {
+	
+	
+	@Autowired
+	TeacherService teacherService;
+	
+	@RequestMapping(path = "getTeacher" , method = RequestMethod.GET)
+	public @ResponseBody List<Teacher> getAllTeacher(){
+		return teacherService.getAllTeacher();
+	}
+	
+	@RequestMapping(path = "/save" , method = RequestMethod.POST)
+	public @ResponseBody Teacher saveTeacher(@RequestBody Teacher teacher) {
+		return teacherService.saveTeacher(teacher);
+	}
+
+	@RequestMapping(path = "/delete/{id}" ,  method =RequestMethod.DELETE)
+	public String deleteTeacher(@PathVariable("id") Integer id) {
+		System.out.println("id:"+id);
+		return teacherService.deleteTeacher(id);
+	}
+	
+	
+	@RequestMapping(path = "/update/{id}" , method = RequestMethod.PUT)
+	public @ResponseBody Teacher updateTeacher(@PathVariable("id") Integer id , @RequestBody Teacher teacher) {
+		return teacherService.updateTeacher(id, teacher);
+	}
+	
+	@RequestMapping(path = "/getTeacherByFirstName" ,  method=RequestMethod.GET)
+	public List<Teacher> getTeacherByFirstName(@RequestParam("firstName") String firstName){
+		return teacherService.getTeacherByFirstName(firstName);
+	}
+}
---
+++ reena_anthony_05_08/Teacher/src/main/java/com/nt/training/SpringBootApplication/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,53 @@
+package com.nt.training.SpringBootApplication.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nt.training.SpringBootApplication.domain.Teacher;
+import com.nt.training.SpringBootApplication.repository.TeacherRepository;
+import com.nt.training.SpringBootApplication.service.TeacherService;
+
+@Service
+public class TeacherServiceImpl implements TeacherService {
+
+	@Autowired
+	TeacherRepository teacherRepository;
+	
+	@Override
+	public List<Teacher> getAllTeacher() {
+		return teacherRepository.getAllTeacher();
+	}
+
+	@Override
+	public Teacher saveTeacher(Teacher teacher) {
+		Teacher stu =teacherRepository.saveTeacher(teacher);
+		return stu;
+	}
+
+	@Override
+	public String deleteTeacher(Integer id) {
+		System.out.println("id:" + id);
+		if(id !=null && id > 0) {
+			System.out.println("To be deleted");
+		return 	teacherRepository.deleteTeacher(id);
+			
+		}else {
+			return "id not valid";
+		}
+	}
+
+	@Override
+	public Teacher updateTeacher(Integer id, Teacher teacher) {
+		// TODO Auto-generated method stub
+		return teacherRepository.updateTeacher(id, teacher);
+	}
+
+	@Override
+	public List<Teacher> getTeacherByFirstName(String firstName) {
+		// TODO Auto-generated method stub
+		return teacherRepository.getTeacherByFirstName(firstName);
+	}
+
+}
