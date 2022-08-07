d11b9a7669852c0de32cf838bdd31059b71e105e
Prakriti-nucleusteq <109510546+Prakriti-nucleusteq@users.noreply.github.com>
2022-08-07 21:59:22 +0530

    Delete Exercise directory

--- Exercise/NamingConvention.java
+++
@@ -1,9 +0,0 @@
-public class NamingConvention {
-    public static void main(String args[]){
-
-
-    String firstName = "Prakriti";
-    //  the java naming conventions is to always start with lower case letter and than capitilize the first letter of every subsequent word
-    int a = 10;
-
-}
