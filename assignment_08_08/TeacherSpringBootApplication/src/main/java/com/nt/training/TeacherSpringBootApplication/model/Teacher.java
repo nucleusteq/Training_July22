5cd5792d07be32ac1adba943c18b7f703413ba3b
Manav Jindal <manav.jindal@nucleusteq.com>
2022-08-08 01:14:35 +0530

    teacher spring boot application

---
+++ assignment_08_08/TeacherSpringBootApplication/src/main/java/com/nt/training/TeacherSpringBootApplication/model/Teacher.java
@@ -0,0 +1,72 @@
+package com.nt.training.TeacherSpringBootApplication.model;
+
+public class Teacher {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String contactNumber;
+	private String emailId;
+	
+	public Teacher() {
+	
+	}
+
+	public Teacher(int id, String firstName, String lastName, String contactNumber, String emailId) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.contactNumber = contactNumber;
+		this.emailId = emailId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	@Override
+	public String toString() {
+		return "Teacher [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", contactNumber="
+				+ contactNumber + ", emailId=" + emailId + "]";
+	}
+	
+	
+	
+}
---
+++ assignment_08_08/TeacherSpringBootApplication/src/main/java/com/nt/training/TeacherSpringBootApplication/repository/impl/TeacherRepositoryImpl.java
@@ -0,0 +1,103 @@
+package com.nt.training.TeacherSpringBootApplication.repository.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.nt.training.TeacherSpringBootApplication.model.Teacher;
+import com.nt.training.TeacherSpringBootApplication.repository.TeacherRepository;
+
+@Repository
+public class TeacherRepositoryImpl implements TeacherRepository {
+
+	private static List<Teacher> teachers;
+	public TeacherRepositoryImpl() {
+		teachers=new ArrayList<Teacher>();
+		teachers.add(new Teacher(1,"Manav","Jindal","9898565475","manav.jindal@gmail.com"));
+	}
+	
+	@Override
+	public Teacher addTeacher(Teacher teacher) {
+		teachers.add(teacher);
+		return teacher;
+	}
+
+	@Override
+	public List<Teacher> getAllTeachers() {
+		return teachers;
+	}
+
+	@Override
+	public String removeTeacher(int id) {
+		int index=-1;
+		for(int i=0;i<teachers.size();i++) {
+			if(teachers.get(i).getId() == id) {
+				index=i;
+			}
+		}
+		if(index>=0) {
+			teachers.remove(index);
+			return "Teacher removed successfully";
+		}
+		
+		return "Teacher with the given id does not exist";
+	}
+
+	@Override
+	public String updateTeacher(int id,Teacher teacher) {
+		System.out.println(id +" "+teacher);
+		boolean flag=false;
+		for(int i=0;i<teachers.size();i++) {
+			System.out.println(id+" "+teachers.get(i).getId()+" "+(teachers.get(i).getId() == id));
+			if(teachers.get(i).getId() == id) {
+				System.out.println(i);
+				Teacher t=teachers.get(i);
+				t.setFirstName(teacher.getFirstName());
+				t.setLastName(teacher.getLastName());
+				t.setContactNumber(teacher.getContactNumber());
+				t.setEmailId(teacher.getEmailId());
+				flag=true;
+			}
+		}
+		if(!flag) {
+			return "Teacher with the given id does not exist";
+		}
+		return "Teacher updated successfully";
+	}
+
+	@Override
+	public Teacher getTeacherFromId(int id) {
+		for(Teacher teacher:teachers) {
+			if(teacher.getId() ==  id) {
+				return teacher;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public List<Teacher> getTeacherFromFirstName(String firstName) {
+		ArrayList<Teacher> teachersWithFirstName=new ArrayList<Teacher>();
+		for(Teacher teacher:teachers) {
+			if(teacher.getFirstName().equalsIgnoreCase(firstName)) {
+				teachersWithFirstName.add(teacher);
+			}
+		}
+		return teachersWithFirstName;
+	}
+	
+	@Override
+	public List<Teacher> getTeacherFromLastName(String lastName) {
+		ArrayList<Teacher> teachersWithLastName=new ArrayList<Teacher>();
+		for(Teacher teacher:teachers) {
+			System.out.println(teacher);
+			if(teacher.getLastName().equalsIgnoreCase(lastName)) {
+				System.out.println(teacher);
+				teachersWithLastName.add(teacher);
+			}
+		}
+		return teachersWithLastName;
+	}
+
+}
