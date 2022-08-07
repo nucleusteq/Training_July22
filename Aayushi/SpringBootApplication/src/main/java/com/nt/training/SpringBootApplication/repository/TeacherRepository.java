d574cb83a2d1ad9b037b44a20ca5501154f7308e
aayushi gopawar <aayushi.gopawar@nucleusteq.com>
2022-08-08 00:20:15 +0530

    teacherCRUD

---
+++ Aayushi/SpringBootApplication/src/main/java/com/nt/training/SpringBootApplication/repository/TeacherRepository.java
@@ -0,0 +1,19 @@
+package com.nt.training.SpringBootApplication.repository;
+
+import java.util.List;
+
+import com.nt.training.SpringBootApplication.domain.Teacher;
+
+public interface TeacherRepository {
+
+	public List<Teacher> getAllTeacher();
+	
+	public Teacher saveTeacher(Teacher student);
+	
+	public String deleteTeacher(Integer id);
+	
+	public Teacher updateTeacher(Integer id , Teacher teacher);
+	
+	public List<Teacher> getTeacherByFirstName(String firstName);
+	
+}
---
+++ Aayushi/SpringBootApplication/src/main/java/com/nt/training/SpringBootApplication/service/TeacherService.java
@@ -0,0 +1,19 @@
+package com.nt.training.SpringBootApplication.service;
+
+import java.util.List;
+
+import com.nt.training.SpringBootApplication.domain.Teacher;
+
+public interface TeacherService {
+
+	public List<Teacher> getAllTeacher();
+	
+	public Teacher saveTeacher(Teacher teacher) ;
+	
+	public String deleteTeacher(Integer id);
+	
+	public Teacher updateTeacher(Integer id, Teacher teacher);
+	
+	public List<Teacher> getTeacherByFirstName(String firstName);
+	
+}
