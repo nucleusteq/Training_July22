65794a3ec7d7fb7f80d1327cebf2ad3c627edba6
Himanshu Dhoke <himanshu.dhoke@nucleusteq.com>
2022-08-10 23:28:04 +0530

    Even odd files added

---
+++ WebDevelopment/JavaScript/CheckEvenOdd/EvenOdd.js
@@ -0,0 +1,14 @@
+var num, temp;
+function fun()
+{
+  num = parseInt(document.getElementById("num").value);
+  if(num)
+  {
+    temp = document.getElementById("resPara");
+    temp.style.display = "block";
+    if(num%2==0)
+      document.getElementById("res").innerHTML = "Even";
+    else
+      document.getElementById("res").innerHTML = "Odd";
+  }
+}
