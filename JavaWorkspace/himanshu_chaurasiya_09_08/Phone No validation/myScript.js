ab9dc5941bdee8230c97d1cf4ee3dbb7153ae41e
Himanshu Chaurasiya <himanshu@nucleusteq.com>
2022-08-11 14:36:41 +0530

    frontend changes

--- JavaWorkspace/himanshu_chaurasiya_09_08/Phone No validation/myScript.js
+++ JavaWorkspace/himanshu_chaurasiya_09_08/Phone No validation/myScript.js
@@ -1,4 +1,6 @@
-function check_num() {
+function check_num() 
+
+{
     let phn = document.getElementById("valid").value;
     let reg = /^[789]\d{9}$/;
     if (phn.match(reg)) {
@@ -8,12 +10,14 @@ function check_num() {
         alert("Please enter a valid number.")
     }
 
-    // let pass = document.getElementById("valid").value;
-    // let reg = /^[789]\d{9}$/;
-    // if (phn.match(reg)) {
-    //     alert("SUCCESS! Nice Number.")
-    // }
-    // else {
-    //     alert("Please enter a valid number.")
-    // }
+    let passw = document.getElementById("pass").value;
+    let co_pass = document.getElementById("conf_pass").value;
+    let pass_reg = /[0-9]{1,}[A-Z a-z]{1,}/;
+    if (passw.match(pass_reg)) {
+
+        passw == co_pass ? alert("SUCCESS! Nice Password.") : alert("Your Password Does Not Match!");
+    }
+    else {
+        alert("Please enter a valid password.");
+    }
 }
