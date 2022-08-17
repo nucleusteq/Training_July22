c29c1a5341aa701ed59c628647252d5abab62a4c
Himanshu Chaurasiya <himanshu@nucleusteq.com>
2022-08-18 00:13:35 +0530

    Vending Machine Done with Database , Changed Response , Query Params

--- JavaWorkspace/himanshu_chaurasiya_06_08/VendingMachine/src/main/java/com/nt/training/VendingMachine/controller/VendingMachineController.java
+++ JavaWorkspace/himanshu_chaurasiya_06_08/VendingMachine/src/main/java/com/nt/training/VendingMachine/controller/VendingMachineController.java
@@ -3,6 +3,7 @@ package com.nt.training.VendingMachine.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import com.nt.training.VendingMachine.domain.ResponseClass;
 import com.nt.training.VendingMachine.domain.VendingMachine;
 import com.nt.training.VendingMachine.service.VendingMachineService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class VendingMachineController {
 	
