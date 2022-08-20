55f19c946176cc66a8774b8163756a4b85c8ea36
Himanshu Chaurasiya <himanshu@nucleusteq.com>
2022-08-20 16:44:34 +0530

    React + Vending Machine , API integration Done with DB connection

---
+++ JavaWorkspace/himanshu_chaurasiya_19_08_practice_only/Readme.txt
@@ -0,0 +1,10 @@
+
+1. To run the above react application , you must have the Vending Machine application running in you local server.
+2. The above react application contains integration of APIs with of project Vending Machine which is in spring boot.
+3. After starting the react application , localhost will be opened automatically.
+4. On firt load , a getAllproduct (GET) API is called , which loads the available products in the Database .
+5. Buy button will call a (GET) api so that user can buy the product  using query params as an input in form.
+6. Add button will call a (POST) api which will add a product in the table connected to database using request body 
+   as an input in form.
+
+** React and Java application , and their functionalities is tested at local level which is working fine . **
---
+++ JavaWorkspace/himanshu_chaurasiya_19_08_practice_only/vending_machine_integration/src/App.js
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+import {
+  GetAllPro,
+  BuyProduct,
+  ProductDetails,
+  AddProductFunc,
+} from "./component/getapidata/getapi";
+
+function App() {
+  useEffect(() => {
+    GetAllProducts();
+  }, []);
+
+  let URL = "http://localhost:8080";
+
+  const [getallpro, setGetAllPro] = useState([]);
+  const [boughtpro, setBoughtPro] = useState({});
+  const [addpro, setAddPro] = useState({});
+
+  const [purchasedproductflag, setPurchasedProductFlag] = useState(false);
+  const [buyproductflag, setBuyProductFlag] = useState(false);
+  const [addproductflag, setAddProductFlag] = useState(false);
+
+
+  const GetAllProducts = async () => {
+    let ENDPOINT = "/getProducts";
+    let res = await axios.get(`${URL}${ENDPOINT}`);
+    console.log("Response from log ", res.data);
+    setGetAllPro(res.data);
+  };
+
+  const BuyPro = async (snackId, inputAmount) => {
+    let ENDPOINT = `/buy?id=${snackId}&amount=${inputAmount}`;
+    let res = await axios.get(`${URL}${ENDPOINT}`);
+    console.log("Response from BuyPro ", res.data);
+    setBoughtPro(res.data);
+    setPurchasedProductFlag(true);
+    setBuyProductFlag(false);
+    GetAllProducts();
+  };
+
+
+  const AddProduct = async (productObj) => {
+    let ENDPOINT = `/add`;
+    console.log("Given Object ", productObj);
+    let res = await axios.post(`${URL}${ENDPOINT}`,productObj);
+    console.log("Response from log ", res.data);
+    setAddPro(res.data);
+    GetAllProducts();
+    setAddProductFlag(false)
+  };
+
+  return (
+    <div className="app-main-div">
+      
+      <div className="first-div">
+        <h2>Welcome to Vending Machine</h2>
+      </div>
+
+      <div className="second-div">
+        <h2>Available Products</h2>
+        <GetAllPro val={getallpro} />
+      </div>
+
+      <div className="third-div">
+
+        <button className="glow-on-hover" onClick={() => {setBuyProductFlag(true);setPurchasedProductFlag(false);setAddProductFlag(false)} }>Buy</button>
+        <button className="glow-on-hover" onClick={() => {setAddProductFlag(true);setBuyProductFlag(false)} }>Add</button>
+
+        {buyproductflag && (
+          <div>
+            <h2>User : Enter Details to Buy Product</h2>
+            <BuyProduct func={BuyPro} />
+          </div>
+        )
+        }
+
+        {addproductflag && (
+          <div>
+            <h2>Admin : Add Product</h2>
+            <AddProductFunc  mainFunc={AddProduct} />
+          </div>
+        )
+        }
+
+      </div>
+        
+      <div className="fourth-div">
+        {purchasedproductflag && (<ProductDetails val={boughtpro} />) }
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
