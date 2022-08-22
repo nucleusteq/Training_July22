eac2c28b3953fb1d84c9184b82a4c90a61a297f3
Himanshu Chaurasiya <himanshu@nucleusteq.com>
2022-08-22 21:35:45 +0530

    React Weather app done

--- JavaWorkspace/himanshu_chaurasiya_18_08/weather_app/src/App.js
+++ JavaWorkspace/himanshu_chaurasiya_18_08/weather_app/src/App.js
@@ -5,11 +5,11 @@ import WeatherReport from "./component/weather/weath";
 function App() {
 
   useEffect(() => {
-  GetCities(cityname);
+  GetCities("Indore");
   },{})
   
-  const [cityname ,setCityName] = useState("Indore");
-  const [citydata, setCityData] = useState({});
+  const [cityname ,setCityName] = useState("");
+  const [citydata, setCityData] = useState(null);
 
   let URL = `https://api.openweathermap.org/data/2.5/weather?q=`
   const GetCities = async (cityname) => {
@@ -17,16 +17,18 @@ function App() {
     URL += ENDURL ;
     let res = await axios.get(`${URL}`);
     setCityData(res.data);
-
-    console.log("Response of URL ", URL);
-    console.log("Response from BuyPro ",res.data);
   };
 
   return (
-      <div>
-            <h2>Welcome to Weather Report</h2>
+      <div className="main-app-div">
+            <h2 id="heading">Welcome to Weather Report</h2>
             <WeatherReport setCityName={setCityName} cityname={cityname} func={GetCities}/>
-            <h2>{citydata.name} Temperatur is {citydata.main.feels_like} C°</h2>
+            <div>
+            {citydata != null ? 
+            <div className="temp-div">
+            <h2>{citydata.name}</h2><h2>{citydata.main.temp} °C</h2>
+             </div>: ""}
+            </div>
       </div>
   );
 }
--- JavaWorkspace/himanshu_chaurasiya_18_08/weather_app/src/component/weather/weath.jsx
+++ JavaWorkspace/himanshu_chaurasiya_18_08/weather_app/src/component/weather/weath.jsx
@@ -1,21 +1,24 @@
+import "../../App.css"
+
 function WeatherReport(props){
 
     function handleSubmit(e) 
   {
     e.preventDefault();
-    props.func(document.getElementById("city").value);   //passing snack id and amount into API required for params
+    props.func(document.getElementById("city").value);  //passing snack id and amount into API required for params
+    document.getElementById("city").value=""
   }
     return(
-        <div>
+        <div className="input-div">
         <form type="submit" onSubmit={handleSubmit}>
             <input id="city" autoFocus
             type="text"
             onChange={(e)=>props.setCityName(e.target.value)}
             placeholder="Enter City"
             />
-
+            <br/><br/>
             <button className="glow-on-hover" type="submit">
-            Submit
+            Change City
             </button>
         </form>
         </div>
