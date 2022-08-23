1bc4bc3107b208cd3973ac5e8dacb2108882e262
Prakriti-nucleusteq <109510546+Prakriti-nucleusteq@users.noreply.github.com>
2022-08-23 16:53:17 +0530

    update weather

---
+++ Weather_app/weatherapp/src/components/Tempapp.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import "./css/style.css";
+
+const Tempapp = () => {
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState("Mumbai");
+
+    useEffect( () => {
+        const fetchApi = async () => {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=5c29e70536325bb7f5ba26d24daeccb0`
+            const response = await fetch(url);
+            const resJson = await response.json();
+            setCity(resJson.main);
+        }
+        fetchApi();
+    },[search] )
+
+    return(
+        <>
+            <div className="box">
+                <div className="inputData">
+                    <input 
+                    type="search" 
+                    value={search}
+                    className="inputField" 
+                    onChange= { (event) => { setSearch(event.target.value)} } />
+                </div>
+
+           {!city ? (
+            <p> no data found</p>           
+            )  : (
+                <div> 
+                <div className="info">
+                    <h2 className="location"> 
+                        <i className="fas fa-street-view">{search }</i>
+                    </h2>
+                    <h1 className="temp">
+                    {city.temp}°Cel
+                    </h1>
+                    <h3 className="tempmin_max">
+                        Min : {city.temp_min}°Cel | Max : {city.temp_max}°Cel
+                    </h3>
+            </div>
+            
+            <div className="wave -one"></div>
+            <div className="wave -two"></div>
+            <div className="wave -three"></div>
+            
+            </div>
+
+            )}
+
+            
+            </div>
+        </>
+    )
+}
+
+export default Tempapp;
