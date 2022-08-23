dbafe2da272d08736a95860b4a970c9439cccb97
Aashi-nucleus <aashi.agrawal@nucleusteq.com>
2022-08-23 15:43:48 +0530

    tasks

---
+++ Testingg/src/com/in/nucleusTeq/jUnit/AddTillNumber.java
@@ -0,0 +1,19 @@
+package com.in.nucleusTeq.jUnit;
+public class AddTillNumber {
+	public String add (Integer num) {
+		if(num == 0)
+			return"Value should not be zero";
+		if(num<0)
+			return "Value should not be less than 1 ";
+		int sum=0;
+		for(int i=num;i>=1;i--)
+		{
+			sum+=i;
+		}
+		return sum+"";
+	}
+	
+	public static void main(String...args) {
+		AddTillNumber add=new AddTillNumber();
+		System.out.println(add.add(4));
+	}}
